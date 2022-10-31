3e2fbfa6cad4aee6601187a073f1b7f0934d48da
RayoAdeoti <adeotiaderayo5@gmail.com>
2022-10-31 16:46:16 +0100

    data

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,5 +1,5 @@
 from email.policy import default
-from tkinter import CASCADE
+
 from django.db import models
 
 # Create your models here.
@@ -7,25 +7,24 @@ from django.db import models
 class Artiste(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField()
 
     def __str__(self):
-        return self.first_name
+        return f"{self.first_name} {self.last_name}"
 
 class Song(models.Model):
-    Artiste = models.ForeignKey('Artiste', null=True, blank=True, on_delete=models.CASCADE)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     date_released = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveIntegerField(default=0)
-    artiste_id = models.CharField(max_length=15)
 
     def __str__(self):
         return self.title
 
 class Lyric(models.Model):
-    Song = models.ForeignKey('Song', null=True, blank=True, on_delete=models.CASCADE)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=150)
-    song_id = models.CharField(max_length=15)
 
     def __str__(self):
         return self.content
+        
